fe1f74c4c67af029528e5913176037666d04a8a7
kon <74819209+kon2300@users.noreply.github.com>
2021-02-23 12:53:48 +0900

    残高表示を実装

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA0U7Hp4zkHRA5AnWn6_hwtHaWq4PL7OxE",
--- src/views/AppMyPage.vue
+++ src/views/AppMyPage.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="app-my-page">
-    <p> {{ $store.getters.getUsername }} さん ようこそ！!</p>
+    <div class="columns m-5 is-size-4">
+      <p class="column"> {{ $store.getters.getUsername }} さん ようこそ！!</p>
+      <p class="column has-text-right">残高: {{$store.getters.getWallet}}</p>
+    </div>
+    <div class="has-text-centered m-3">
+      <h1 class="title h1">ユーザ一覧</h1>
+      <table>
+        <tr>
+          <th>ユーザ名</th>
+        </tr>
+        <tbody></tbody>
+      </table>
+    </div>
   </div>
 </template>
 
